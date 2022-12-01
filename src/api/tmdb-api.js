5d0bb661c1fa815f2659c41ed11ecaa36a2d6bbf
Keith Butler <20089137@mail.wit.ie>
2022-12-01 15:30:07 +0000

    added Actor endpoint and view

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -86,3 +86,69 @@ export const getUpcomingMovies = () => {
      throw error
   });
 };
+
+// TODO gets popular people not people in general
+export const getActors = () => {
+  // console.log("Getting Actors");
+  return fetch(
+    `https:api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    // `https:api.themoviedb.org/3/person/latest?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    // `https:api.themoviedb.org/3/discover/people?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error
+  });
+};
+
+export const getActor = (args) => {
+  const [, idPart] = args.queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error
+  });
+};
+
+export const getActorImages = ({ queryKey }) => {
+  const [, idPart] = queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/person/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then( (response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    throw error
+ });
+};
+
+export const getCombinedCredits = async (args) => {
+  const [, idPart] = args.queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/person/${id}/combined_credits?api_key=${process.env.REACT_APP_TMDB_KEY}"&language=en-US`
+  ).then( (response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    throw error
+ });
+};
---
+++ src/components/actorCard/index.js
@@ -0,0 +1,87 @@
+import React, { useContext  } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CardHeader from "@mui/material/CardHeader";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+// import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+// import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import IconButton from "@mui/material/IconButton";
+import Grid from "@mui/material/Grid";
+import img from '../../images/film-poster-placeholder.png';
+import { Link } from "react-router-dom";
+import Avatar from '@mui/material/Avatar';
+import { ActorsContext } from "../../contexts/actorsContext";
+
+
+export default function ActorCard({ actor, action }) {
+  // const { favourites, addToFavourites } = useContext(ActorsContext);
+
+
+  // if (favourites.find((id) => id === actor.id)) {
+  //   actor.favourite = true;
+  // } else {
+  //   actor.favourite = false
+  // }
+
+  // const handleAddToFavourite = (e) => {
+  //   e.preventDefault();
+  //   addToFavourites(actor);
+  // };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+          avatar={
+            actor.favourite ? (
+              <Avatar sx={{ backgroundColor: 'red' }}>
+                {action(actor)}
+              </Avatar>
+            ): null
+          }
+          name={
+            <Typography variant="h5" component="p">
+              {actor.name}{" "}
+            </Typography>
+          }
+      />
+      <CardMedia
+        sx={{ height: 500 }}
+        image={
+          actor.profile_path
+            ? `https://image.tmdb.org/t/p/w500/${actor.profile_path}`
+            : img
+        }
+      />
+      <CardContent>
+        <Grid container>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <StarRateIcon fontSize="small" />
+              {"  "}{actor.popularity}{" "}
+            </Typography>
+            {/* <Typography variant="h6" component="p"> */}
+            {/*   {actor.known_for_department} */}
+            {/* </Typography> */}
+          </Grid>
+          <Grid item xs={6}>
+          </Grid>
+        </Grid>
+      </CardContent>
+      <CardActions disableSpacing>
+        {/* <IconButton aria-label="add to favourites" onClick={handleAddToFavourite}> */}
+        {/*   <FavoriteIcon color="primary" fontSize="large" /> */}
+        {/* </IconButton> */}
+        {/* <Link to={`/actors/${actor.id}`}> */}
+        {/*   <Button variant="outlined" size="medium" color="primary"> */}
+        {/*     More Info ... */}
+        {/*   </Button> */}
+        {/* </Link> */}
+      </CardActions>
+    </Card>
+  );
+}
