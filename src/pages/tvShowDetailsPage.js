7e42fe14dbf57255cc31e723c1035327549ede04
Keith Butler <20089137@mail.wit.ie>
2022-12-02 19:21:35 +0000

    Renamed Import

--- src/pages/tvShowDetailsPage.js
+++ src/pages/tvShowDetailsPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from 'react-router-dom';
 import TvShowDetails from "../components/tvShowDetails/";
-import PageTemplate from "../components/templateMoviePage";
+import PageTemplate from "../components/templateTvShowPage";
 import { getTvShow } from '../api/tmdb-api'
 import { useQuery } from "react-query";
 import Spinner from '../components/spinner'
