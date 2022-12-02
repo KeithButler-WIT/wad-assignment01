fe3c3f3d03aa5bd78528f652e80136d0c5d14c66
Keith Butler <20089137@mail.wit.ie>
2022-12-02 16:39:45 +0000

    Update homePage.js

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -37,7 +37,7 @@ const HomePage = (props) => {
         return (
           <>
             <AddToFavouritesIcon movie={movie} />
-            <AddToPlaylist movie={movie} />
+            <AddToPlaylistIcon movie={movie} />
           </>
         );
       }}
